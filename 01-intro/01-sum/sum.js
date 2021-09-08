e87a90191c1e9b1436c41fdefb2447a4d6b55ffb
Alexander <petrushin64@ya.ru>
2021-09-08 17:42:57 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a === "number" && typeof b === "number") {
+
+    return a + b;
+  } else {
+
+    throw new TypeError;
+  }
+
 }
 
 module.exports = sum;
