cecfb82844e7484edfb93cbe77111f68a5cf53ca
Alexander <petrushin64@ya.ru>
2021-09-20 14:51:11 +0300

    rewrited task 4

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,9 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const LimitSizeStream = require('./LimitSizeStream');
+const fs = require('fs');
+const {EEXIST} = require('constants');
 
 const server = new http.Server();
 
@@ -12,7 +15,63 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end();
+      } else {
+        const limitedStream = new LimitSizeStream({limit: 1e6}); // 1 Мегабайт
 
+        const writeStream = fs.createWriteStream(filepath, {
+          flags: 'wx',
+        });
+
+        writeStream.on('error', (err) => {
+          if (err.code === 'EEXIST') {
+            res.statusCode = 409;
+            res.end('file already exists');
+            return;
+          }
+          res.statusCode = 500;
+          res.end();
+        });
+
+        writeStream.on('finish', () => {
+          res.statusCode = 201;
+          res.end('file saved');
+        });
+
+        req.pipe(limitedStream).pipe(writeStream);
+
+        limitedStream.on('error', (err) => {
+          if (err.code === 'LIMIT_EXCEEDED') {
+            // fs.rmSync(filepath, { force: true });
+            res.statusCode = 413;
+            res.end();
+            fs.unlink(filepath, (err) => {
+              if (err) throw err;
+              console.log(`${filepath} was deleted`);
+            });
+            return;
+          }
+          res.statusCode = 500;
+          res.end();
+        });
+
+        req.on('aborted', () => {
+          limitedStream.destroy();
+          writeStream.destroy();
+          fs.unlink(filepath, (e) => {
+            console.log(
+                'Соединение было прервано, файл не сохранён',
+                e.message,
+            );
+          });
+        });
+
+        req.on('error', (error) => {
+          console.log(error);
+        });
+      }
       break;
 
     default:
