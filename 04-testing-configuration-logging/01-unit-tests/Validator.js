9d1bb6695e7b53053e59e898eed0489a2881d350
Alexander <petrushin64@ya.ru>
2021-09-20 18:40:36 +0300

    Исправлен валидатор, написаны тесты

--- 04-testing-configuration-logging/01-unit-tests/Validator.js
+++ 04-testing-configuration-logging/01-unit-tests/Validator.js
@@ -12,6 +12,15 @@ module.exports = class Validator {
       const value = obj[field];
       const type = typeof value;
 
+      if (value === undefined) {
+        return errors;
+      }
+
+      if (type !== 'string' && type !== 'number') {
+        errors.push({field, error: `expect string or number, got ${type}`});
+        return errors;
+      }
+
       if (type !== rules.type) {
         errors.push({field, error: `expect ${rules.type}, got ${type}`});
         return errors;
@@ -27,11 +36,17 @@ module.exports = class Validator {
           }
           break;
         case 'number':
+          if (isNaN(value)) {
+            errors.push({field, error: `expected number, got NaN`});
+          }
+          if (value === Math.abs(Infinity)) {
+            errors.push({field, error: `expected number, got Infinity`});
+          }
           if (value < rules.min) {
             errors.push({field, error: `too little, expect ${rules.min}, got ${value}`});
           }
           if (value > rules.max) {
-            errors.push({field, error: `too big, expect ${rules.min}, got ${value}`});
+            errors.push({field, error: `too big, expect ${rules.max}, got ${value}`});
           }
           break;
       }
